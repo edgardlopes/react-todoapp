208646bf17c60297cb4f3158ca41c93f4a79c452
Edgard 1010257 <edgard.lopes@leucotron.com.br>
2018-04-16 16:54:58 -0300

    migrando db para MongoAtlas, finalizando front-end sem Redux

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
+const url = 'mongodb://todoapp:ePm2P5Ce1HZwRZNS@cluster0-shard-00-00-a6srg.mongodb.net:27017,cluster0-shard-00-01-a6srg.mongodb.net:27017,cluster0-shard-00-02-a6srg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+//module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect(url, {
+    useMongoClient: true
+})
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+import IconButton from '../template/iconButton'
+
+export default props => {
+    const renderRows = () => {
+        const list = props.list || []
+        return (
+            list.map(todo => (
+                <tr key={todo._id}>
+                    <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+                    <td>
+                        <IconButton hide={todo.done} icon='check' style="success" onClick={() => props.handleMarkAsDone(todo)}/>
+                        <IconButton hide={!todo.done} icon='undo' style="warning" onClick={() => props.handleMarkAsPending(todo)}/>
+                        <IconButton hide={!todo.done} icon='trash-o' style="danger" onClick={() => props.handleRemove(todo)}/>
+                    </td>
+                </tr>
+            ))
+        )
+    }
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th className="tableActions">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
