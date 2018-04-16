6e967479297f50163ed28aaf477dd32733d32963
Edgard Lopes <edgard-rodrigo@hotmail.com>
2018-04-16 11:44:36 -0300

    first commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import PageHeader from '../template/pageHeader'
+
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa historia</h2>
+        <p>balablabalblbla</p>
+    </div>
+)
